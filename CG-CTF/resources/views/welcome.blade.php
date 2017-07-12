1625a4ae93ff9b62fbbfbbc5ee47dd263de98a31
wjbsyc <wjbsyc@hotmail.com>
2017-07-13 00:11:14 +0800

    test

--- CG-CTF/resources/views/welcome.blade.php
+++ CG-CTF/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
