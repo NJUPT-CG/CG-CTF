9eec651441ec796b464d55839d958201277811af
wjbsyc <wjbsyc@hotmail.com>
2017-07-15 01:48:37 +0800

    增加密码重置功能
    密码重置的邮箱密码并没有上传,还有一个问题就是发送的重置邮件链接是hostname/password/reset/{token}可能与本地的路径有所不同

--- CG-CTF/config/mail.php
+++ CG-CTF/config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'mail.homura.cc'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@mails.homura.cc'),
+        'name' => env('MAIL_FROM_NAME', 'admin'),
     ],
 
     /*
@@ -111,6 +111,13 @@ return [
     | of the emails. Or, you may simply stick with the Laravel defaults!
     |
     */
+    'stream' => [
+    'ssl' => [
+        'allow_self_signed' => true,
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ],
+],
 
     'markdown' => [
         'theme' => 'default',
