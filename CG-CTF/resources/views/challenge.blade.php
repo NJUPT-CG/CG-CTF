367241adae7d3564c8fdb198720eeb5781f7c5e7
otakekumi <960596293@qq.com>
2017-07-18 22:01:47 +0800

    修改模态框渲染的数据

--- CG-CTF/resources/views/challenge.blade.php
+++ CG-CTF/resources/views/challenge.blade.php
@@ -40,7 +40,7 @@
 <div id="challenge-container">
     @if(isset($challengeInfo))
         @foreach($challengeInfo as $challenge)
-        <button type="button" id="challenge{{$challenge['id']}}" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+        <button type="button" id="challenge{{$challenge['id']}}" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#challenges{{$challenge['id']}}">
 
             <p>{{$challenge['title']}}</p><br>
             <p>{{$challenge['score']}}</p>
@@ -50,22 +50,28 @@
     @endif
 </div>
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="ChallengeTitle">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="ChallengeTitle">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Submit</button>
-      </div>
-    </div>
-  </div>
-</div>
+@if(isset($challengeInfo))
+    @foreach($challengeInfo as $challenge)
+        <div class="modal fade" id="challenges{{$challenge['id']}}" tabindex="-1" role="dialog" aria-labelledby="ChallengeTitle">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h3 class="modal-title" id="ChallengeTitle">{{$challenge['title']}}</h3>
+                        <h5 class="modal-title" id="ChallengeTitle">{{$challenge['score']}}</h5>
+                    </div>
+                    <div class="modal-body">
+                        {{$challenge['info']}}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    @endif
+
 </body>
 </html>
