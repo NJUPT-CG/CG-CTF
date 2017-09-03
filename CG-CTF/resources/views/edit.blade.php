60ec93e7e1df7f5f9bab12c09a47d8fd77d96932
wjbsyc <wjbsyc@hotmail.com>
2017-09-04 00:40:58 +0800

    .....

--- CG-CTF/resources/views/edit.blade.php
+++ CG-CTF/resources/views/edit.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>  
-<html lang="en">  
-<head>  
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>CG-CTF</title>
-
-    <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-</head>
-    <body>
+@section('content')
     <div class="container">  
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
@@ -51,5 +40,4 @@
         </div>
     </div>
 </div>  
-</body>  
-</html>  
+@endsection  
