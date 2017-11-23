be570cb52a15a39db2812c75d970a9c5ac7cde06
wjbsyc <wjbsyc@hotmail.com>
2017-11-23 20:08:35 +0800

    https?

--- CG-CTF/app/Providers/AppServiceProvider.php
+++ CG-CTF/app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('APP_HTTPS')){
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
